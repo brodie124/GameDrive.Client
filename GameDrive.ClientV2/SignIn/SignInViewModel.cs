5fe190877fc91d56ef4d20558cef038c8314de56
Brodie Pestell <brodie.pestell7@gmail.com>
2023-06-23 14:57:46 +0100

    Add ViewModel for SignIn with necessary bindings

--- GameDrive.ClientV2/SignIn/SignInViewModel.cs
+++ GameDrive.ClientV2/SignIn/SignInViewModel.cs
@@ -1,6 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace GameDrive.ClientV2.SignIn;
 
-public class SignInViewModel
+public class SignInViewModel : INotifyPropertyChanged
 {
+    private string _username = string.Empty;
+    private string _password = string.Empty;
     
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string Username
+    {
+        get => _username;
+        set => SetField(ref _username, value);
+    }
+
+    public string Password
+    {
+        get => _password;
+        set => SetField(ref _password, value);
+    }
+
+
+
+    public void DoSignIn()
+    {
+        Console.WriteLine("Do sign in here...");
+    }
+
+
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 }
--- GameDrive.ClientV2/SignIn/SignInWindow.xaml.cs
+++ GameDrive.ClientV2/SignIn/SignInWindow.xaml.cs
@@ -4,8 +4,16 @@ namespace GameDrive.ClientV2.SignIn;
 
 public partial class SignInWindow : Window
 {
+    private SignInViewModel _viewModel;
+    
     public SignInWindow()
     {
         InitializeComponent();
+        _viewModel = (SignInViewModel) DataContext;
+    }
+
+    private void OnSignInClicked(object sender, RoutedEventArgs e)
+    {
+        _viewModel.DoSignIn();
     }
 }

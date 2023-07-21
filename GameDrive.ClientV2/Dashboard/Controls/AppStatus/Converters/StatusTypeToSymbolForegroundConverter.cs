2e5f9ef5061095297fcd0ac4135ed64aebb669de
Brodie Pestell <brodie.pestell7@gmail.com>
2023-07-21 16:16:58 +0100

    Remove redundant comments

--- GameDrive.ClientV2/Dashboard/Controls/AppStatus/Converters/StatusTypeToSymbolForegroundConverter.cs
+++ GameDrive.ClientV2/Dashboard/Controls/AppStatus/Converters/StatusTypeToSymbolForegroundConverter.cs
@@ -27,7 +27,6 @@ public class StatusTypeToSymbolForegroundConverter  : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        // throw new NotImplementedException();
         return value;
     }
 
